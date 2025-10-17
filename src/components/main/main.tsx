71e7b12d92921a0e58d69dae1154c53c842f0c9d
melissawebster <imelissawebster@gmail.com>
2025-10-17 18:56:34 -0300

    add border brown

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -5,14 +5,14 @@ export default function Main() {
   return (
     <main
       className="flex-1 mx-auto mt-[56px] md:[64px] lg:mt-[72px] container bg-gunmetal flex flex-col 
-      max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 
+      max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl
+      border-brown 
       py-3 px-4 sm:px-6 md:px-12"
     >
-      <div className="flex flex-col gap-y-4 mt-6 md:mt-8 ">
+      <div className="flex flex-col gap-y-4 mt-6 -m-2 md:mt-8 ">
         <MainHeader />
         <MainList />
       </div>
-
     </main>
   );
 }
