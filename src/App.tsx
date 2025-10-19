5d8ef9caa184fb32e971a3b706b09ad80b129acd
melissawebster <imelissawebster@gmail.com>
2025-10-19 16:24:08 -0300

    fix build issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import Header from "./components/Header";
+import Header from "./components/header";
 import "./App.css";
-import Footer from "./components/Footer";
-import Main from "./components/main/Main";
+import Footer from "./components/footer";
+import Main from "./components/main/main";
 
 export default function App() {
   return (
