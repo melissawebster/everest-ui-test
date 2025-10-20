aeb7b1c4fcdf486de8634401a66e3f82739d0ce6
melissawebster <imelissawebster@gmail.com>
2025-10-20 11:38:35 -0300

    fix border brown issue

--- src/components/layout/Container.tsx
+++ src/components/layout/Container.tsx
@@ -16,7 +16,7 @@ export default function Container() {
     <main
       className="flex-1 mx-auto mt-[56px] md:[64px] lg:mt-[72px] container bg-gunmetal flex flex-col 
       max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 
-      md:border-brown pt-4 pb-12 px-5 md:px-12"
+      border-brown pt-4 pb-12 px-5 md:px-12"
     >
       <div className="flex flex-col gap-y-10 mt-6 -m-2 md:mt-8 ">
         <ToDoHeader onAdd={handleAddTodo} />
