449dfe60e2d3478825935424146b69720016fc7f
melissawebster <imelissawebster@gmail.com>
2026-01-23 16:26:17 -0300

    update empty message

--- src/components/todos/EmptyData.tsx
+++ src/components/todos/EmptyData.tsx
@@ -2,7 +2,6 @@ export function EmptyData() {
   return (
     <div className="flex flex-col min-h-25 border border-dashed rounded-md justify-center items-center">
       <p>Nothing here for now.</p>
-      <p>Let&apos;s change that!</p>
     </div>
   );
 }
